e30cfdf455f13881d6e2fe681228b978d37fbb23
zhangxv_0 <8153942+zhangxv0@user.noreply.gitee.com>
2021-04-03 12:00:57 +0800

    主函数

--- main.py
+++ main.py
@@ -13,3 +13,7 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+import os
+
+a = os.listdir(path='c:/')
+print(a)
