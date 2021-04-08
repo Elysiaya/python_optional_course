b86d9ba6900156222eac6ea1e565da4f4724671d
zhangxv_0 <8153942+zhangxv0@user.noreply.gitee.com>
2021-04-08 14:56:21 +0800

    利用plot画图

--- 利用开源框架开发.py
+++ 利用开源框架开发.py
@@ -3,8 +3,11 @@ import matplotlib.pyplot as plt
 
 x1 = np.linspace(0.0, 5.0)
 x2 = np.linspace(0.0, 2.0)
+# x3 = np.linspace(-5,5)
+
 y1 = np.cos(2 * np.pi * x1) * np.exp(-x1)
 y2 = np.cos(2 * np.pi * x2)
+# y3 = np.cos(x3)
 
 plt.subplot(2, 1, 1)
 plt.plot(x1, y1, 'o-')
@@ -15,5 +18,10 @@ plt.subplot(2, 1, 2)
 plt.plot(x2, y2, '.-')
 plt.xlabel('time (s)')
 plt.ylabel('Undamped')
+
+# plt.subplot(3, 1, 3)
+# plt.plot(x3, y3, '.-')
+# plt.xlabel('x')
+# plt.ylabel('y')
 
 plt.show()
