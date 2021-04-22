f1f61db9a0d41504d2d0f1bc5274d14d0da07be2
zhangxv_0 <8153942+zhangxv0@user.noreply.gitee.com>
2021-04-22 13:46:02 +0800

    资料

--- request.py
+++ request.py
@@ -3,6 +3,7 @@ from lxml import etree
 import re
 import json
 import os
+
 bv = 'BV1m54y1b72h'
 url = 'https://www.bilibili.com/video/' + bv
 headers = {
@@ -10,30 +11,6 @@ headers = {
                   "Safari/537.36 Edg/89.0.774.76",
     "referer": "https://manga.bilibili.com/"
 }
-r = requests.get(url=url, headers=headers)
-
-html = etree.HTML(r.text)
-
-r = html.xpath('/html/head/script[5]/text()')[0]
-
-re_pattern = re.compile('{.+}')
-r_json = re.findall(pattern=re_pattern, string=r)[0]
-
-j = json.loads(r_json)
-
-data = j["data"]
-
-quality = data['quality']
-format = data['format']
-timelength = data['timelength']
-accept_format = data['accept_format']
-accept_description = data['accept_description']
-accept_quality = data['accept_quality']
-
-video = data['dash']['video'][0]['baseUrl']
-audio = data['dash']['audio'][0]['baseUrl']
-print(video)
-print(audio)
 
 
 def download(url, path):
@@ -41,6 +18,8 @@ def download(url, path):
     with open(path, 'wb') as f:
         f.write(r.content)
     return path
+
+
 def video_add_mp3(video_path, audio_path):
     """
      视频添加音频
@@ -49,7 +28,7 @@ def video_add_mp3(video_path, audio_path):
     :return:
     """
     outfile_name = video_path.split('.')[0] + '.mp4'
-    path='ffmpeg.exe' + ' -i ' + '\"' + video_path + '\"' + ' -i ' + '\"' + audio_path + '\"' + ' -codec copy ' + '\"' + outfile_name + '\"'
+    path = 'ffmpeg.exe' + ' -i ' + '\"' + video_path + '\"' + ' -i ' + '\"' + audio_path + '\"' + ' -codec copy ' + '\"' + outfile_name + '\"'
     os.system(path)
     if os.path.exists(outfile_name):
         os.remove(video_path)
@@ -59,6 +38,29 @@ def video_add_mp3(video_path, audio_path):
         print('视频合成失败')
 
 
-p1=download(video, bv + '_video.m4s')
-p2=download(audio, bv + '_audio.m4s')
-video_add_mp3(p1,p2)
+if __name__ == '__main__':
+    r = requests.get(url=url, headers=headers)
+
+    html = etree.HTML(r.text)
+
+    r = html.xpath('/html/head/script[5]/text()')[0]
+
+    re_pattern = re.compile('{.+}')
+    r_json = re.findall(pattern=re_pattern, string=r)[0]
+
+    j = json.loads(r_json)
+
+    data = j["data"]
+
+    quality = data['quality']
+    format = data['format']
+    timelength = data['timelength']
+    accept_format = data['accept_format']
+    accept_description = data['accept_description']
+    accept_quality = data['accept_quality']
+
+    video = data['dash']['video'][0]['baseUrl']
+    audio = data['dash']['audio'][0]['baseUrl']
+    p1 = download(video, bv + '_video.m4s')
+    p2 = download(audio, bv + '_audio.m4s')
+    video_add_mp3(p1, p2)
